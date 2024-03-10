264022afccd3b69d5e9f6b8693eade3f593abb77
yberikov <berikov04@gmail.com>
2024-03-11 00:50:40 +0500

    validtion

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/ajg/form v1.5.1 // indirect
+	github.com/badoux/checkmail v1.2.4 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/crypto v0.21.0 // indirect
--- internal/http-server/handlers/register/register.go
+++ internal/http-server/handlers/register/register.go
@@ -3,6 +3,8 @@ package register
 import (
 	"context"
 	"encoding/json"
+	"errors"
+	"github.com/badoux/checkmail"
 	"github.com/go-chi/render"
 	"golang.org/x/crypto/bcrypt"
 	"net/http"
@@ -23,8 +25,8 @@ func New(auth storage.Auth) http.HandlerFunc {
 			render.JSON(w, r, response.Error(`invalid json request`))
 			return
 		}
-		if createUserRequest.Email == "" {
-			render.JSON(w, r, response.Error("Empty"))
+		if err := validate(createUserRequest); err != nil {
+			render.JSON(w, r, response.Error(err.Error()))
 			return
 		}
 		hash, err := bcrypt.GenerateFromPassword([]byte(createUserRequest.Password), bcrypt.MinCost)
@@ -39,4 +41,20 @@ func New(auth storage.Auth) http.HandlerFunc {
 
 		render.JSON(w, r, response.OK())
 	}
+}
+
+func validate(req Request) error {
+	if req.Email == "" {
+		return errors.New("email cannot be empty")
+	}
+
+	if err := checkmail.ValidateFormat(req.Email); err != nil {
+		return errors.New("invalid email format")
+	}
+
+	if req.Password == "" {
+		return errors.New("password cannot be empty")
+	}
+	// You can add more validation rules here, like minimum password length
+	return nil
 }
